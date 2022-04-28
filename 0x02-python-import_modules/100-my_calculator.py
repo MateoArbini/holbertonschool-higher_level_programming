bc8579d34806e4764291564030ce297a20bcede9
MateoArbini <mateoarbini1998@gmail.com>
2022-04-28 08:49:10 -0700

    Task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    sum = 0
+    argc = len(sys.argv)
+    argv = sys.argv
+    if (argc - 1 != 3 ):
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        
+        if(argv[2] == "+"):
+            print(f"{a} {argv[2]} {b} = {add(a, b)}")
+        elif(argv[2] == "-"):
+            print(f"{a} {argv[2]} {b} = {sub(a, b)}")
+        elif(argv[2] == "*"):
+            print(f"{a} {argv[2]} {b} = {mul(a, b)}")
+        elif(argv[2] == "/"):
+            print(f"{a} {argv[2]} {b} = {div(a, b)}")
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
