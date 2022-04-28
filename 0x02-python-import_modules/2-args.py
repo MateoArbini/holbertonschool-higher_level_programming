ecc83238f0132213dd517783d2e557bd08125f59
MateoArbini <mateoarbini1998@gmail.com>
2022-04-28 08:03:33 -0700

    Task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     if((len(sys.argv) - 1) == 0):
-        print(f"{len(sys.argv) - 1} argument.")
+        print(f"{len(sys.argv) - 1} arguments.")
     elif((len(sys.argv) - 1 == 1)):
         print(f"{len(sys.argv) - 1} argument:")
     else:
