77ee62ab51b65a6e69554b8e1f090a343479a24d
MateoArbini <mateoarbini1998@gmail.com>
2022-05-02 09:29:13 -0700

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        length = len(i)
-        count = 0
-        for j in i:
-            print(j, end='')
-            if count <= length - 1:
-                print(" ", end='')
-                count += 1
-        print("$")
+    a = [[]]
+    if(type(a) == type(matrix)):
+        for i in matrix:
+            for j in i:
+                print(f"{j:d}", end=' ')
+            print('', end='\n')
