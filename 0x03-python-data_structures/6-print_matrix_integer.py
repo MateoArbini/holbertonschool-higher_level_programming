98fd79c8b229968fc708eafc49b9575c715b42ac
MateoArbini <mateoarbini1998@gmail.com>
2022-05-02 08:04:22 -0700

    Task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for i in matrix:
+        length = len(i)
+        count = 0
+        for j in i:
+            print(j, end='')
+            if count < length - 1:
+                print(" ", end='')
+                count += 1
+        print("$")
