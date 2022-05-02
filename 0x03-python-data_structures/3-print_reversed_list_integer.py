f5064114d93a203d5bae782eaf6c6d64dd362e2d
MateoArbini <mateoarbini1998@gmail.com>
2022-05-02 09:09:49 -0700

    Task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -4,4 +4,4 @@ def print_reversed_list_integer(my_list=[]):
     if(type(a) == type(my_list)):
         my_list.reverse()
         for i in my_list:
-            print(i, sep="\n")
+            print(f"{i:d}", sep="\n")
