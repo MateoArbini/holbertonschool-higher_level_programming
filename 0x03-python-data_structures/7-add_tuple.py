676f4e091584933c8a00ed85c43dfa0eefc31afc
MateoArbini <mateoarbini1998@gmail.com>
2022-05-02 10:44:40 -0700

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,11 +5,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
     length_b = len(tuple_b)
     if(type(tuple_c) == type(tuple_a) and type(tuple_c) == type(tuple_b)):
         if(length_a < 2 and length_a != 0):
-            for i in range(0, length_a):
+            while length_a < 2:
                 tuple_a = tuple_a + (0, )
+                length_a += 1
         elif(length_b < 2 and length_b != 0):
-            for j in range(0, length_b):
+            while length_b < 2:
                 tuple_b = tuple_b + (0, )
+                length_b += 1
         elif(length_a == 0):
             tuple_a = tuple_a + (0, 0)
         elif(length_b == 0):
