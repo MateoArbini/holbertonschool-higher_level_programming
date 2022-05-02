6bbf6fa2d43e597d550ef2332bd5010f6e9ac3f3
MateoArbini <mateoarbini1998@gmail.com>
2022-05-02 10:47:56 -0700

    Task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -3,23 +3,18 @@ def add_tuple(tuple_a=(), tuple_b=()):
     tuple_c = ()
     length_a = len(tuple_a)
     length_b = len(tuple_b)
-    if(type(tuple_c) == type(tuple_a) and type(tuple_c) == type(tuple_b)):
-        if(length_a < 2 and length_a != 0):
-            while length_a < 2:
-                tuple_a = tuple_a + (0, )
-                length_a += 1
-        elif(length_b < 2 and length_b != 0):
-            while length_b < 2:
-                tuple_b = tuple_b + (0, )
-                length_b += 1
-        elif(length_a == 0):
-            tuple_a = tuple_a + (0, 0)
-        elif(length_b == 0):
-            tuple_b = tuple_b + (0, 0)
-        elif(length_a > 2):
-            tuple_a = (tuple_a[0], tuple_a[1])
-        elif(length_b > 2):
-            tuple_b = (tuple_b[0], tuple_b[1])
+    if(length_a < 2 and length_a != 0):
+        while length_a < 2:
+            tuple_a = tuple_a + (0, )
+            length_a += 1
+    elif(length_b < 2 and length_b != 0):
+        while length_b < 2:
+            tuple_b = tuple_b + (0, )
+            length_b += 1
+    elif(length_a == 0):
+        tuple_a = tuple_a + (0, 0)
+    elif(length_b == 0):
+        tuple_b = tuple_b + (0, 0)
 
     for h in tuple_a:
         for k in tuple_b:
