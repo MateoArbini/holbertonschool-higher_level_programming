f1c13e0071a84cd57001f1d4943718e3d8dc149d
MateoArbini <mateoarbini1998@gmail.com>
2022-05-02 11:13:42 -0700

    task 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
     length = len(sentence)
     first = sentence[0]
     if(length == 0):
-        return 0, None
+        return (0, None)
 
     added_value_tuple = (length,)
     added_value_tuple2 = (first,)
