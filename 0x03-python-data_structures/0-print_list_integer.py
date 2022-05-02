d9c73e92ae36f94a70ad1d28e2d4a902c872ba06
MateoArbini <mateoarbini1998@gmail.com>
2022-05-02 08:41:39 -0700

    task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@ def print_list_integer(my_list=[]):
     a = []
     if type(a) == type(my_list):
         for i in my_list:
-            print(f"{i}", end='\n')
+            print(f"{i:d}", end='\n')
