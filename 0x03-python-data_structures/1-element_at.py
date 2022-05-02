e9c02ec85a260ae36d7c1aeb137c79db091844bd
MateoArbini <mateoarbini1998@gmail.com>
2022-05-02 09:02:32 -0700

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,9 +3,9 @@ def element_at(my_list, idx):
     length = len(my_list)
     a = []
     if(type(a) == type(my_list)):
-            if(idx < 0):
-                return None
-            elif(idx >= length):
-                return None
-            else:
-                return my_list[idx]
+        if(idx < 0):
+            return None
+        elif(idx >= length):
+            return None
+        else:
+            return my_list[idx]
