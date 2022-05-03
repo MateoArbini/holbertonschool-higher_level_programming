9ffd553f1fdd579333610af6cc2849b0399cadf2
MateoArbini <mateoarbini1998@gmail.com>
2022-05-03 05:25:38 -0700

    Task 5

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -2,3 +2,8 @@
 str = "Holberton School"
 print("{}".format(str * 3))
 print("{}".format(str[0:9]))
+
+'''
+print 3 times a string stored in the variable str,
+followed by its first 9 characters.
+'''
