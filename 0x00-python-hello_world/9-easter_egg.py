875dad089c8c2ab477a2768b6fafce41090f7e77
MateoArbini <mateoarbini1998@gmail.com>
2022-05-03 05:26:50 -0700

    Task 9

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
 import this
+
+'''
+Python script that prints “The Zen of Python”, by TimPeters,
+followed by a new line. script should be maximum 98 characters
+long (please check with wc -m 9-easter_egg.py.
+'''
