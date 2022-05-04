b1a69f475bc104417d95886725910c33907f0757
MateoArbini <mateoarbini1998@gmail.com>
2022-05-04 08:14:45 -0700

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     def potencia(lista):
-        result = list(map(lambda x: x ** 2 , lista))
+        result = list(map(lambda x: x ** 2, lista))
         return result
     final = list(map(potencia, matrix))
     return final
-
