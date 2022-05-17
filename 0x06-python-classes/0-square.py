454e1b877786fd0a8aa28cf42e6747ecf140a7a6
MateoArbini <mateoarbini1998@gmail.com>
2022-05-17 08:09:01 -0700

    task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''function that creates a new class named Square'''
+class Square:
+    '''Here we create a new class named "Square"'''
+    def __init__(self):
+        '''Initializing the class "Person"'''
+        pass
