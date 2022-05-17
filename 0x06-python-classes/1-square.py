1b17100581b567d5a2cbbd3c526aa4d75139a562
MateoArbini <mateoarbini1998@gmail.com>
2022-05-17 08:36:37 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''function that creates a class named "Square", which is defined by
+a private instance attribute called "size", and instantiation with
+"size" (no type/value verification'''
+class Square:
+    '''Here we create a new class named "Square"'''
+    def __init__(self, __size):
+        '''Here we initialize the class created, withe the parameters
+        "self", and size. This last parameter, is a private attribute,
+        and that is why is has two "__" before the name. The reason why
+        this attribute is private, it is because the size of a square is
+        crucial for a square and many things depend on it. So we need to take
+        the control of it and keep it privately.'''
+        self.__size = __size
+
