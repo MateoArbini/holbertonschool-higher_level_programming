4d32ef4986cc20983a16cce471d68718e94ffcd3
MateoArbini <mateoarbini1998@gmail.com>
2022-05-18 07:13:46 -0700

    task 2

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+'''function that creates a class named "Recatangle'''
+
+class Rectangle:
+    '''Here we create the class'''
+    def __init__(self, width=0, height=0):
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        '''private instance attribute named width'''
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        '''setter of the private instance width'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        '''private instance attribute named height'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''setter of the private instance height'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        '''public method to calculate the area of the rectangle'''
+        return self.__width * self.__height
+
+    def perimeter(self):
+        '''public method to calculate the perimeter of the rectangle'''
+        result = 0
+        if self.__width <= 0 or self.__height <= 0:
+            result = 0
+        else:
+            result = (self.__width * 2) + (self.__height * 2)
+        return result
