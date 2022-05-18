8fc6ae55fb0a6fc61dc9f065709243c5e42ab3cf
MateoArbini <mateoarbini1998@gmail.com>
2022-05-18 14:50:09 -0700

    task 8

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -90,7 +90,7 @@ class Rectangle:
         elif object2 is False:
             raise TypeError("rect_2 must be an instance of Rectangle")
 
-        if rect_1 >= rect_2:
+        if rect_1.area() >= rect_2.area():
             return rect_1
         else:
             return rect_2
