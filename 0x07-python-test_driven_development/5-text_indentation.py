bdbfb5b97a2b3a760e07811a81dda634f44468a8
MateoArbini <mateoarbini1998@gmail.com>
2022-05-22 14:33:03 -0700

    task5 with pycodestyle

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,6 +4,7 @@ function that prints a text with 2 new lines after each of these
 characters: ., ? and :
 '''
 
+
 def text_indentation(text):
     '''function'''
     if type(text) is not str:
@@ -12,4 +13,4 @@ def text_indentation(text):
     new_string = text.replace('. ', '.\n\n')
     new_string1 = new_string.replace('? ', '?\n\n')
     new_string2 = new_string1.replace(': ', ':\n\n')
-    print(new_string2, end='') 
+    print(new_string2, end='')
