d9e279f8eeb66583ccf70a2927a0fb4e408b4584
MateoArbini <mateoarbini1998@gmail.com>
2022-05-23 14:01:14 -0700

    new test case added

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -16,6 +16,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1, -2, -64, -34]), -1)
         self.assertEqual(max_integer([3, 3, 3]), 3)
         self.assertEqual(max_integer([5]), 5)
-
+        '''testing case when a variable named "x" is created and it has
+        a value of 5 and it is inserted into the list'''
+        x = 10
+        self.assertEqual(max_integer([1, 2, 3, 10]), 10)
+    
     if __name__ == "__main__":
         unittest.main()
