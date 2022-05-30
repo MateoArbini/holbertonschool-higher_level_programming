55507c81a0e892095377575f69d6e7d340293268
MateoArbini <mateoarbini1998@gmail.com>
2022-05-30 08:08:36 -0700

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Write a class BaseGeometry (based on 5-base_geometry.py).
+Public instance method: def area(self): that raises an
+Exception with the message area() is not implemented
+'''
+
+
+class BaseGeometry:
+    '''class'''
+    def __init__(self):
+        '''initializing the class basegeometry'''
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
