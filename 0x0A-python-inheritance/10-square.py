05f1996a4abf201b11b83b99c56cac885dd22f55
MateoArbini <mateoarbini1998@gmail.com>
2022-05-30 14:26:15 -0700

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -6,43 +6,12 @@ Instantiation with size: def __init__(self, size)::
 the area() method must be implemented'''
 
 
-class BaseGeometry:
-    '''class'''
-    def __init__(self):
-        '''initializing the class basegeometry'''
-
-    def area(self):
-        '''public instance method not implemented'''
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        '''public instance method'''
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-class Rectangle(BaseGeometry):
-    '''class inheritance'''
-    def __init__(self, width, height):
-        '''initializing class'''
-        super().integer_validator("width", width)
-        self.__width = width
-        super().integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        '''function that returns the area'''
-        return self.__height * self.__width
-
-    def __str__(self):
-        '''function that return a string'''
-        return (f"[Rectangle] {self.__width}/{self.__height}")
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     '''class inheritance from Rectangle'''
     def __init__(self, size):
         '''initializing class'''
-        super().__init__(size, size)
+        super().integer_validator("size", size)
+        self.__size = size
