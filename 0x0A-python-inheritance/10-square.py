ff1566dc190aab5352fcda3fa4ec12c467a90ecf
MateoArbini <mateoarbini1998@gmail.com>
2022-05-30 11:57:56 -0700

    task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -45,13 +45,4 @@ class Square(Rectangle):
     '''class inheritance from Rectangle'''
     def __init__(self, size):
         '''initializing class'''
-        super().integer_validator("size", size)
-        self.__size = size
-
-    def area(self):
-        '''function that returns the area of a square'''
-        return self.__size * self.__size
-
-    def __str__(self):
-        '''function that return a string'''
-        return (f"[Rectangle] {self.__size}/{self.__size}")
+        super().__init__(size, size)
