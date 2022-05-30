a6a0b06311e7e695098f5b9545120566eb423071
MateoArbini <mateoarbini1998@gmail.com>
2022-05-30 07:29:12 -0700

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''create a class named "MyList" that inherits from list. Also, create a public
+instance method called def print_sorted(self):  that prints the list, but
+sorted (ascending sort).
+'''
+
+
+class MyList(list):
+    '''class'''
+    def print_sorted(self):
+        '''function to print the list sorted'''
+        print(sorted(self))
