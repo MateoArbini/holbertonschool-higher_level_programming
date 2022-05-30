5ced7a887d3827f18da7005289df7b48a02f19dc
MateoArbini <mateoarbini1998@gmail.com>
2022-05-30 09:26:12 -0700

    task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+'''following the steps of the previous excercise, add a new instance method
+called def integer_validator(self, name, value):'''
+
+
+class BaseGeometry:
+    '''class'''
+    def __init__(self):
+        '''initializing the class basegeometry'''
+
+    def area(self):
+        '''public instance method not implemented'''
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        '''public instance method'''
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
