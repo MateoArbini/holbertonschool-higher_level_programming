67e260ff1a65c7f4cd40a7fa7c1403380086e633
MateoArbini <mateoarbini1998@gmail.com>
2022-05-31 13:47:26 -0700

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-import json
-from sys import argv
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 '''
 Write a script that adds all arguments to a Python list, and then save
 them to a file:
@@ -15,6 +11,12 @@ them to a file:
     If the file doesn’t exist, it should be created
     You don’t need to manage file permissions / exceptions.
 '''
+
+
+import json
+from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
 fname = "add_item.json"
