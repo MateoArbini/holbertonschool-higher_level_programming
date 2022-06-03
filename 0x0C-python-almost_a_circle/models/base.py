9523dd1d1a364ed5a2f86578b4a737ea4ae33545
MateoArbini <mateoarbini1998@gmail.com>
2022-06-03 09:58:45 -0700

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,6 +21,9 @@ Write the first class Base:
 '''
 
 
+import json
+
+
 class Base:
     '''class'''
     __nb_objects = 0
@@ -32,3 +35,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        '''returns the JSON string representation of list_dictionaries'''
+        if list_dictionaries is None or len(list_dictionaries):
+            return str(json.dumps(list_dictionaries))
+        else:
+            return json.dumps(list_dictionaries)
