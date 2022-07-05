44b877883c7429fe7e7a76da244c8214256a82fc
MateoArbini <mateoarbini1998@gmail.com>
2022-07-05 09:18:09 -0700

    task5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- script that prints the description of the table first_name from the database
+-- bhtn_0c_0, and the name will be passed as an argument. You are not allowed
+-- to use the DESCRIBE or EXPLAIN statements
+SHOW CREATE TABLE first_table
