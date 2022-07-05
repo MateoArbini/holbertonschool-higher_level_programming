64d5751f2b2a078ad3f1dca93d2e7691735b9264
MateoArbini <mateoarbini1998@gmail.com>
2022-07-05 10:11:15 -0700

    task9

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,28 @@
+-- create a second table in the database hbtn_0c_0 and add multiplerows
+-- description: id = INT, name = VARCHAR(256), score(INT)
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT INTO second_table VALUES 
+(
+	1,
+	"John",
+	10
+),
+(
+	2,
+	"Alex",
+	3
+),
+(
+	3,
+	"Bob",
+	14
+),
+(
+	4,
+	"George",
+	8
+);
