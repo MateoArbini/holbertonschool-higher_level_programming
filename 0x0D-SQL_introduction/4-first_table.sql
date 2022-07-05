8333c64031536a01a7e6d8a7d0fdd4343d153554
MateoArbini <mateoarbini1998@gmail.com>
2022-07-05 08:50:21 -0700

    task4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,6 @@
-/*
- * Write a script that creates a table called first_table in the current database in your MySQL server.
- * first_table description:
- * id INT
- * name VARCHAR(256)
- */
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+-- Create a table if it doesnt exist yet, that must be called first_name and
+-- the description must be, id - INT and name - VARCHAR(256). Show or select
+-- are not allowed to use
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT, 
+	name VARCHAR(256));
