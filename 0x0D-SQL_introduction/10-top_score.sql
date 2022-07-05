1c500ab5ff7ae0b7a5442e2d923510aa21aedbd8
MateoArbini <mateoarbini1998@gmail.com>
2022-07-05 10:19:07 -0700

    task10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- script that lists all records of the table second_table of the database
+-- results should be ordered both the score and the name. Records should be
+-- ordered by score(top first)
+SELECT score, name
+FROM second_table
+ORDER BY score DESC
