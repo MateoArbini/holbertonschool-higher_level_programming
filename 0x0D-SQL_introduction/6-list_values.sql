8edd4a272f15fd2ff87895e2454ca55b120b9d05
MateoArbini <mateoarbini1998@gmail.com>
2022-07-05 09:22:11 -0700

    task6

---
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -0,0 +1,4 @@
+-- script that lists all rows of the table first_table form database hbtn_0c_0
+-- All fields should be printed. The database name will be passed as an arg
+SELECT *
+FROM first_table
