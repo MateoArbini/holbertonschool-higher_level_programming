1e30abe1310ded5319700c3febb531014e5eabee
MateoArbini <mateoarbini1998@gmail.com>
2022-07-05 09:43:27 -0700

    task8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,5 @@
 -- script that display the numer of records with id = 89 in the table
 -- first_table of the database hbtn_0c_0.
-SELECT count(id) 
+SELECT count(*) 
 FROM first_table
-WHERE 89
+WHERE id = 89
