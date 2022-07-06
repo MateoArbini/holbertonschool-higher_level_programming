054bb6bbe427a41776c06238a0b175f54283d4a0
MateoArbini <mateoarbini1998@gmail.com>
2022-07-06 12:32:13 -0700

    task10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,6 +4,6 @@
 -- tv_shows.title and tv_show_genres.genre_id. You can use only one SELECT
 -- statement.
 SELECT tv_shows.title, tv_show_genres.genre_id
-FROM hbtn_0d_tvshows
-LEFT JOIN tv_show_genres ON tv_shows.title = tv_show_genres.genre_id
-ORDER BY tv_shows.title and tv_show_genres.genre_id ASC
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC
