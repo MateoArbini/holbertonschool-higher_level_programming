db700e1dbdbdaf84f97a5f099f1ab23cecb5cfcd
MateoArbini <mateoarbini1998@gmail.com>
2022-07-06 11:53:57 -0700

    task10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -5,5 +5,5 @@
 -- statement.
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM hbtn_0d_tvshows
-LEFT JOIN genre_id ON tv_shows.title = tv_show_genres.genre_id
+LEFT JOIN tv_show_genres ON tv_shows.title = tv_show_genres.genre_id
 ORDER BY tv_shows.title and tv_show_genres.genre_id ASC
