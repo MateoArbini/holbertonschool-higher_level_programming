eea83b63d6249f4e58cf8dddc4ce073938147c50
MateoArbini <mateoarbini1998@gmail.com>
2022-07-06 11:03:11 -0700

    task8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,9 +2,9 @@
 -- database hbtn_0d_usa. The states table contains only one record where
 -- name = California (but the id can be different, as per the example) Results
 -- must be sorted in ascending order by cities.id Join not allowed.
-SELECT cities
+SELECT id, name
 FROM hbtn_0d_usa
-WHERE cities.id = (
+WHERE state_id = (
 	SELECT id
 	FROM states
 	WHERE name = "California");
