e9de7d0b0378a55bcefa24ba3c8176cc7b5feffd
MateoArbini <mateoarbini1998@gmail.com>
2022-07-06 15:16:14 -0700

    task15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -3,7 +3,7 @@
 -- (but the id can be different). Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
 -- You can use only one SELECT statement
-SELECT tv_shows.title as title
+SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
