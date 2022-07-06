f96b642ccc97847c694a00ac122ffeb442736ad5
MateoArbini <mateoarbini1998@gmail.com>
2022-07-06 08:12:06 -0700

    task3

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -3,5 +3,6 @@
 -- will be passed as an argument of the mysql command. If the table force_name
 -- already exists, your script should not fail.
 CREATE TABLE IF NOT EXISTS force_name (
-	id INT
-	name VARCHAR(256) NOT NULL);
+	id INT,
+	name VARCHAR(256) NOT NULL
+);
