4deb2312a0366424bc7eec9696a4f953f448d012
MateoArbini <mateoarbini1998@gmail.com>
2022-08-01 14:16:34 -0700

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,15 +3,17 @@
 /* script that searches the second biggest integer in the list of arguments */
 
 const lista = [];
+let unicos = [];
 let x = 2;
 
 for (x; x < process.argv.length; x++) {
   lista.push(process.argv[x]);
 }
 lista.sort();
+unicos = [... new Set(lista)];
 
-if (lista.length === 1 || !process.argv[2]) {
+if (unicos.length === 1 || !process.argv[2]) {
   console.log('0');
 } else {
-  console.log(lista[lista.length - 2]);
+  console.log(unicos[unicos.length - 2]);
 }
