f916d136b13c87816fc2f0e6cda1fdfe1cc25554
MateoArbini <mateoarbini1998@gmail.com>
2022-08-01 10:22:29 -0700

    task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,19 +3,13 @@
 /* script that prints a square */
 
 const size = process.argv[2];
-const x = 'X'
-let string = "";
+const x = 'X';
 let y = 0;
-let j = 0;
 
-if (isNaN(size))
-{
-	console.log('Missing size');
-}
-else
-	parseInt(size);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else { parseInt(size); }
 
-for (y; y < size; y++)
-{
-	console.log(x.repeat(size));
+for (y; y < size; y++) {
+  console.log(x.repeat(size));
 }
