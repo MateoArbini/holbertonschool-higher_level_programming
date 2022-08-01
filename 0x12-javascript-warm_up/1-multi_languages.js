0708ae338e07bc918c72e3e4a2c080cae49dcd5f
MateoArbini <mateoarbini1998@gmail.com>
2022-08-01 06:40:37 -0700

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* script that prints 3 lines */
+
+const aa = 'C is fun';
+const bb = 'Python is cool';
+const cc = 'JavaScript is amazing';
+console.log(aa);
+console.log(bb);
+console.log(cc);
