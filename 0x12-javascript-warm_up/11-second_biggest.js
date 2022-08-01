4a3fb9506bbfbca58c3f87452813f68011956999
MateoArbini <mateoarbini1998@gmail.com>
2022-08-01 14:03:49 -0700

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,27 +4,14 @@
 
 const lista = [];
 let x = 2;
-let y = 0;
 
-for (x; x < process.argv.length; x++) 
-{
-	lista.push(process.argv[x]);
+for (x; x < process.argv.length; x++) {
+  lista.push(process.argv[x]);
 }
 lista.sort();
 
-if (lista.length == 0)
-{
-	console.log('0');
-}
-else if (lista.length == 1 && lista[0] == 0)
-{
-	console.log('0');
-}
-else if (lista.length == 1 && lista[0] == 1)
-{
-	console.log('0');
-}
-else
-{
-	console.log(lista[lista.length - 2]);
+if (lista.length === 1 || !process.argv[2]) {
+  console.log('0');
+} else {
+  console.log(lista[lista.length - 2]);
 }
