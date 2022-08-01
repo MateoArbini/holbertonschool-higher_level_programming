f0c1d653b57f5253475ed7413804ed09b610ac54
MateoArbini <mateoarbini1998@gmail.com>
2022-08-01 08:30:32 -0700

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* script that prints 3 lines but by using an array of string and a loop */
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let x = 0;
+
+for (x; x < arr.length; x++) {
+  console.log(arr[x]);
+}
