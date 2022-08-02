d02873308c7f4970504b7d420ab0296ae177b34d
MateoArbini <mateoarbini1998@gmail.com>
2022-08-02 12:10:24 -0700

    task 10

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+/* function that converts a number from base 10 to another base passed as argument */
+
+exports.converter = function (base) {
+	function x (num) {
+		return (num.toString(base));
+	}
+	return x;
+}
