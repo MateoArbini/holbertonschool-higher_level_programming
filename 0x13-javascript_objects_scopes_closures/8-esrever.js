9ee775a82c6ac28026d55c0f30581a1672280b47
MateoArbini <mateoarbini1998@gmail.com>
2022-08-02 10:29:52 -0700

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,11 +3,10 @@
 /* function that returns the reversed version of a list */
 
 exports.esrever = function (list) {
-	let rev = [];
-	let x = list.length - 1;
-	for (x; x >= 0; x--)
-	{
-		rev.push(list[x]);
-	}
-	return rev;
-}
+  const rev = [];
+  let x = list.length - 1;
+  for (x; x >= 0; x--) {
+    rev.push(list[x]);
+  }
+  return rev;
+};
