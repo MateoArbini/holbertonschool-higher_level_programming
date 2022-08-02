fed1324d6b1bebf3a2d77bb7b9daa8919cc0a237
MateoArbini <mateoarbini1998@gmail.com>
2022-08-02 09:21:44 -0700

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -5,17 +5,23 @@
 const Rectangle = require('./4-rectangle');
 
 class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
-  charPrint (c) {
-    let x = 0;
-    if (c === undefined) {
-      for (x; x < this.size; x++) { console.log('X'.repeat(this.size)); }
-    } else {
-      for (x; x < this.size; x++) { console.log('C'.repeat(this.size)); }
-    }
-  }
+	constructor(size) {
+		super(size, size);
+		this.size = size;
+	}
+	charPrint (c) {
+		let x = 0;
+		if (c === undefined)
+		{
+			for (x; x < this.size; x++)
+				console.log('X'.repeat(this.size));
+		}
+		else
+		{
+			for (x; x < this.size; x++)
+				console.log('C'.repeat(this.size));
+		}
+	}
 }
+
 module.exports = Square;
