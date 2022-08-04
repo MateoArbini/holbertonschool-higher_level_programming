0f5b3bb81411d97f99f337e8bcbe3f98fe52b074
MateoArbini <mateoarbini1998@gmail.com>
2022-08-04 08:22:49 -0700

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+'''script that list states passed as arg from the database hbtn_0e_0_usa but
+taking care of code injection'''
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    try:
+        user = sys.argv[1]
+        passw = sys.argv[2]
+        db = sys.argv[3]
+        state_name = sys.argv[4]
+        conn = MySQLdb.connect(host="localhost", port=3306, user=f"{user}",
+                               passwd=f"{passw}", db=f"{db}", charset="utf8")
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM states \
+                    WHERE name LIKE '%s' ORDER BY id ASC" % (state_name))
+        query_rows = cur.fetchall()
+        for row in query_rows:
+            print(row)
+        cur.close()
+        conn.close()
+    except Excpetion as error:
+        print("Error with connection to DB")
