fc43fa2105169461efbab5d4c363ab290dbd6feb
Mateo <mateoarbini1998@gmail.com>
2022-08-18 15:05:47 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -4,5 +4,4 @@
 # with the flag -s, we disable the progression display
 # with the grep, we filter the output, in order that it only show the content
 # with the awk, we filter the content of the length, so we only get the number
-
 curl -sI "$1" | grep Content-Length | awk '{print $2}'
