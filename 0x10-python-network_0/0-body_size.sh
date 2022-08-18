edce022cc36061b9407a15b20db08bf6abe6b42d
Mateo <mateoarbini1998@gmail.com>
2022-08-18 15:07:29 +0000

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# script that request and display the size of the body of that response
-# with the flag -I, we obtain only headers
-# with the flag -s, we disable the progression display
-# with the grep, we filter the output, in order that it only show the content
-# with the awk, we filter the content of the length, so we only get the number
+# bash script that prints the content length in bytes
 curl -sI "$1" | grep Content-Length | awk '{print $2}'
