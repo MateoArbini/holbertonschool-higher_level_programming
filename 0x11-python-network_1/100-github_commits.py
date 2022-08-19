936d1ebefdeb84c72c6401b06c58feba316c2a2d
Mateo <mateoarbini1998@gmail.com>
2022-08-19 20:29:16 +0000

    task 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,17 +9,12 @@ if __name__ == "__main__":
 
     repo = argv[1]
     user = argv[2]
-    cont = 0
-
     url = 'https://api.github.com/repos/{}/{}/commits'.format(user, repo)
-
     res = requests.get(url)
     json = res.json()
-
-    for commit in json:
+    cont = 0
+    for x in json:
         if cont < 10:
-            print(commit.get('sha'), end=': ')
-            print(commit.get('commit').get('author').get('user'))
+            print(x.get('sha'), end=': ')
+            print(x.get('commit').get('author').get('name'))
             cont += 1
-        else:
-            break
