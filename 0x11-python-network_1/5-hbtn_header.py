40aafb0e15ef93af1ea6c282818df48d7d07bf33
Mateo <mateoarbini1998@gmail.com>
2022-08-19 17:27:04 +0000

    task 5

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,12 +4,12 @@ script that takes an url, sends a request to the url and displays the value of
 the variable X-Request-Id in the response header
 '''
 
-import requests
+import requests as req
 from sys import argv
 
 
 if __name__ == "__main__":
     url = argv[1]
 
-    resp = requests.head(url)
-    print(resp.headers['X-Request-Id'])
+    resp = req.head(url)
+    print(resp.headers.get['X-Request-Id'])
