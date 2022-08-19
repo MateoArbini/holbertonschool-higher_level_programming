7e68d0cbfe1c467f12199b4a05ec96ca3297b2c5
Mateo <mateoarbini1998@gmail.com>
2022-08-19 15:09:55 +0000

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,7 @@
 '''script that takes an url, sends a request to the url and displays
 the value of the X-Request-Id variable found in the header of the response'''
 
+
 import urllib.request
 import sys
 
