17d1acc44411c4fc116f9d6230f42f20cdffb197
MateoArbini <mateoarbini1998@gmail.com>
2022-09-12 13:38:21 +0000

    task1 with semi ok

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,6 +5,7 @@ The first argument is the file path
 The second argument is the string to write
 The content of the file must be written in utf-8
 If an error occurred during while writing, print the error object
+Page: "https://nodejs.dev/en/learn/writing-files-with-nodejs/"
 */
 
 const fs = require('fs');
