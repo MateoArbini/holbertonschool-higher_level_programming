e66cb82c86385d1b27629eb415008480e7bbfe18
MateoArbini <mateoarbini1998@gmail.com>
2022-09-12 18:10:30 +0000

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,23 +7,23 @@ You must use the module axios
 */
 
 const axios = require('axios');
-const url = process.argv[2]
+const url = process.argv[2];
 
 axios.get(url)
   .then(response => {
     const dic = {}; // dic to return
-    let data = response.data; // var with the data of tasks of each user
-    for (let x of data) { // going through each data
-        if (x.completed === true) { //if the tasks is completed
-            if (dic[x.userId] !== undefined) { //if user is already in dic
-                const task_completed = dic[x.userId];
-                dic[x.userId] = task_completed + 1;//here we set the value
-            } else {
-                dic[x.userId] = 1; // if user not in dic, it must be added
-                // and we must add the new user with the correspondant value
-                // of tasks
-            }
-          }
+    const data = response.data; // var with the data of tasks of each user
+    for (const x of data) { // going through each data
+      if (x.completed === true) { // if the tasks is completed
+        if (dic[x.userId] !== undefined) { // if user is already in dic
+          const status = dic[x.userId];
+          dic[x.userId] = status + 1;// here we set the value
+        } else {
+          dic[x.userId] = 1; // if user not in dic, it must be added
+          // and we must add the new user with the correspondant value
+          // of tasks
         }
-        console.log(dic) //print the dic
-    })
+      }
+    }
+    console.log(dic); // print the dic
+  });
