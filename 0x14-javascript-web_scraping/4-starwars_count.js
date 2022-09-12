c9589626f9d994ca5a3ea6e18d463048698635cf
MateoArbini <mateoarbini1998@gmail.com>
2022-09-12 15:46:03 +0000

    task4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+/*
+Write a script that prints the number of movies where the character
+“Wedge Antilles” is present:
+The first argument is the API URL of the
+Star wars API: https://swapi-api.hbtn.io/api/films/
+Wedge Antilles is character ID 18 - your script must use
+this ID for filtering the result of the API
+You must use the module axios
+*/
+
+const axios = require('axios');
+
+axios.get('https://swapi-api.hbtn.io/api/films/')
+  .then(response => {
+    let totalmovies = 0;
+    const movies = response.data.results;
+    for (const x in movies) {
+      const characters = movies[x].characters;
+      for (const j in characters) {
+        if (characters[j].includes('18')) {
+          totalmovies = totalmovies + 1;
+          break;
+        }
+      }
+    }
+    console.log(totalmovies);
+  })
+  .catch(error => {
+    console.log(error.response.status);
+  });
