1a950a33617daf76fc90013edddc795129e41962
MateoArbini <mateoarbini1998@gmail.com>
2022-09-12 13:37:50 +0000

    task1 with semi ok

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /*
-Write a script that writes a string to a file:
+Write a script that writes a string to a file;
 The first argument is the file path
 The second argument is the string to write
 The content of the file must be written in utf-8
