3327b43157a4838b750431e796f6f1247d559362
MateoArbini <mateoarbini1998@gmail.com>
2022-09-19 12:38:24 +0000

    task 0

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// With the queryselector, we select the tag header and store it in a var "x"
+// then we asign the color that we want!
+const x = document.querySelector('header');
+x.style.color = '#FF0000';
